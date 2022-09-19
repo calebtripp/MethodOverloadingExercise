30d2c3de023cdd8d6b21e2cf6965d3129d88b5fc
calebtripp <calebtripp@outlook.com>
2022-09-19 12:33:24 -0400

    exerciseComplete

---
+++ MethodOverloadingExercise/MethodOverloadingExercise/Methods.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloadingExercise
+{
+    internal class Methods
+    {
+        public static int Add(int a, int b)
+        { 
+            return a + b; 
+        }
+        public static decimal Add(decimal a, decimal b)
+        { 
+            return a + b; 
+        }
+        public static string Add(int a, int b, bool c)
+        {
+            if (c == true && (a+b) <=1)
+            {
+                return ($"{a + b} dollar");
+            }
+            else if (c == true && (a + b) >1)
+            {
+                return ($"{a + b} dollars");
+            }
+            else
+            {
+                return "Falsey false false";
+            }
+                     
+        }
+    }
+}
---
+++ MethodOverloadingExercise/MethodOverloadingExercise/Program.cs
@@ -0,0 +1,4 @@
+﻿using MethodOverloadingExercise;
+
+var answerOne = Methods.Add(0, 1, true);
+Console.WriteLine(answerOne);
